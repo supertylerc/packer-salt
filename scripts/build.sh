c921f537a563c7a6c5e899e3757fd4bdc1d36e07
Tyler Christiansen <code@tylerc.me>
2017-11-06 19:27:33 -0500

    update the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -91,8 +91,19 @@ function backup() {
   cp /etc/salt/minion /etc/salt/minion.bak
 }
 
+function setup_ssh_key() {
+  mkdir -p /home/vagrant/.ssh/
+  curl -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
+  chmod 0700 /home/vagrant/.ssh/
+  chmod 0600 /home/vagrant/.ssh/authorized_keys
+  chown -R vagrant:vagrant /home/vagrant/.ssh/
+}
+
 function main() {
+  setup_ssh_key
   setup_yum
+  dependencies
+  guest_additions
   install
   backup
 }
